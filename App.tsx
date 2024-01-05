ec9deb356714faa5626cbf8f35438699ba9f4b91
umanand triapthi <umanandblog@gmail.com>
2024-01-05 16:31:24 +0530

    background Changer

--- App.tsx
+++ App.tsx
@@ -5,113 +5,117 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useEffect, useState} from 'react';
+
 import {
-  SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [buttonCLicked, setButtonCLicked] = useState(false);
+  const [randomBackground, setRandomBackground] = useState('#ffffff');
+  const [box1randomBackground, setbox1RandomBackground] = useState('#FFC0CB');
+  const [box2randomBackground, setbox2RandomBackground] = useState('#F5F5DC');
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  useEffect(() => {
+    if (buttonCLicked) {
+      const bigBox = () => {
+        const hexRange = '0123456789ABCDEF';
+        let color = '#';
+        for (let index = 0; index < 6; index++) {
+          color += hexRange[Math.floor(Math.random() * 16)];
+        }
+        setRandomBackground(color);
+      };
+
+      const Box1 = () => {
+        const hexRange = '0123456789ABCDEF';
+        let color = '#';
+        for (let index = 0; index < 6; index++) {
+          color += hexRange[Math.floor(Math.random() * 16)];
+        }
+        setbox1RandomBackground(color);
+      };
+
+      const Box2 = () => {
+        const hexRange = '0123456789ABCDEF';
+        let color = '#';
+        for (let index = 0; index < 6; index++) {
+          color += hexRange[Math.floor(Math.random() * 16)];
+        }
+        setbox2RandomBackground(color);
+      };
+      bigBox();
+      Box1();
+      Box2();
+    }
+  },[buttonCLicked]);
+
+  const generateColor = () => {
+    setButtonCLicked(true);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <>
+      <StatusBar backgroundColor={randomBackground} />
+
+      <View style={[styles.container, {backgroundColor: randomBackground}]}>
+        <View style={[styles.box1, {backgroundColor: box1randomBackground}]}>
+          <Text> Hii</Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+
+        <View style={[styles.box2, {backgroundColor: box2randomBackground}]}>
+          <Text> Hii</Text>
+        </View>
+
+        <TouchableOpacity onPress={generateColor}>
+          <View style={styles.actionBtn}>
+            <Text style={styles.actionBtnTxt}> Press me</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
-  sectionTitle: {
+
+  box1: {
+    backgroundColor: 'pink',
+    width: 100,
+    height: 100,
+    borderWidth: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  box2: {
+    backgroundColor: 'beige',
+    width: 100,
+    height: 100,
+    borderWidth: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  actionBtn: {
+    borderRadius: 12,
+    backgroundColor: '#6A1B4D',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+  },
+  actionBtnTxt: {
     fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    color: '#FFFFFF',
+    textTransform: 'uppercase',
   },
 });
 
